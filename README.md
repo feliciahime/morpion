30e625c2c825c259b493dee0dbd701ee4043aa60
Felicia Becerra <16847624+feliciahime@users.noreply.github.com>
2021-03-04 17:11:25 -0800

    Language selector and instructions working

--- README.md
+++ README.md
@@ -11,26 +11,3 @@ Purple snow: #909cb1
 Pink: #cbbebb
 Alt-pink: #bfb2af
 
-
-
-
-	// let title = document.querySelector('#title');
-	// console.log('Current title:', title);
-
-
-// let title = document.querySelector('#title');
-
-// function testMe() {
-// 	console.log('Getting translation..');
-// 	let title = document.querySelector('#title');
-// 	console.log('title', title);
-// }
-
-	// let title = document.querySelector('#title');
-	// 	if (title.textContent === En_title ) {
- //        	title.textContent = Fr_title; 
- //        	console.log('Translating to French...');
- //        	} else {
- //        		title.textContent = En_title; 
- //        		console.log("Translating to English.");
- //        	}
--- js/main.js
+++ js/main.js
@@ -3,25 +3,29 @@ console.log('Getting started.');
 
 let En_title = "Tic-Tac-Toe";
 let Fr_title = "Jeu de Morpion";
+let En_inst = "Click on a tile to play. The first player to set 3 tiles in a row wins.";
+let Fr_inst = "Cliquez sur une tuile pour jouer. Le premier joueur a aligner 3 symboles identiques gagne la partie.";
 
 
-
-console.log(En_title, Fr_title, title);
+console.log(En_title, Fr_title, En_inst, Fr_inst);
 
 
 function changeLanguage() {
 	console.log('Button getting clicked!')
-	let title = document.querySelector('#title');
-	let choice = document.querySelector('#lang-Choice');
+	let title = document.querySelector('#game-Title');
+	let choice = document.querySelector('#game-Lang');
+	let instructions = document.querySelector('#game-Instr');
 	if ( title.textContent === En_title ) {
 		console.log('Translating to French...');
 		title.textContent = Fr_title; 
 		choice.textContent = 'Anglais / English';
+		instructions.textContent = Fr_inst;
 
 		} else {
 			console.log("Translating to English.");
 			title.textContent = En_title;
 			choice.textContent = 'Français';
+			instructions.textContent = En_inst;
 			 }
 }
 
